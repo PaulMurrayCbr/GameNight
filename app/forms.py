938ffc59eaf8244a251f4987dcb09cde52abdad2
Paul Murray <pmurray@bigpond.com>
2017-04-22 15:40:38 +1000

    Checkbox fields are ugly, but whatevs.

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,19 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, IntegerField, RadioField
 from wtforms.validators import DataRequired
 
-class PC(Form):
+class PC(FlaskForm):
     abbrev = StringField('abbrev', validators=[DataRequired()])
     name = StringField('name', validators=[DataRequired()])
     pname = StringField('pname', validators=[DataRequired()])
 
-class HP(Form):
+class HP(FlaskForm):
     source = StringField('source')
     max = IntegerField('max')
     current = IntegerField('current')
     ablative_only = BooleanField('ablative_only')
 
-class NewBuff(Form):
+class NewBuff(FlaskForm):
     description = StringField('description')
     duration_unit = RadioField('duration_unit', choices=['rd', 'mn', 'hr', 'dy'])
     duration = IntegerField('duration')
